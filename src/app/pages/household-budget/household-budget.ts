473d0c4710b7c0368104f54f41461f5bc6f79e9a
Carolina Fantini <carolfantinirocha@gmail.com>
2025-09-25 16:32:05 -0300

    criando estrutura template do household budget

--- src/app/pages/household-budget/household-budget.ts
+++ src/app/pages/household-budget/household-budget.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterGlobal } from '../../common/footer-global/footer-global';
 import { MenuGlobal } from '../../common/menu-global/menu-global';
+import { ChartsService } from '../../services/charts-service';
 
 @Component({
   selector: 'app-household-budget',
@@ -9,6 +10,18 @@ import { MenuGlobal } from '../../common/menu-global/menu-global';
   templateUrl: './household-budget.html',
   styleUrl: './household-budget.scss'
 })
-export class HouseholdBudget {
+export class HouseholdBudget implements AfterViewInit {
+  titulo: string = 'Household Budget';
+  @ViewChild('chartContainer', { static: true }) chartContainer!: ElementRef;
 
+  constructor(private chartService: ChartsService) { }
+
+  ngAfterViewInit(): void {
+    this.chartService.renderLineChart(
+      this.chartContainer.nativeElement,
+      'Vendas',
+      ['Jan', 'Fev', 'Mar'],
+      [{ name: 'Produto A', data: [10, 20, 30] }]
+    );
+  }
 }
