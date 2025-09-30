43b819eb7220d263f2d8f5e7e537d7592e684864
Carolina Fantini <carolfantinirocha@gmail.com>
2025-09-30 15:59:35 -0300

    removendo infos não usadas de algumas classes

--- src/app/models/reading-journal/author.ts
+++ src/app/models/reading-journal/author.ts
@@ -3,7 +3,6 @@ import { Book } from "./book";
 export class Author {
     id!: number;
     name?: string;
-    books?: Book[];
 
     constructor(init?: Partial<Author>) {
         Object.assign(this, init);
--- src/app/models/reading-journal/series.ts
+++ src/app/models/reading-journal/series.ts
@@ -4,8 +4,6 @@ import { Book } from "./book";
 export class Series {
   id!: number;
   name?: string;
-  author?: Author;
-  books?: Book[];
 
   constructor(init?: Partial<Series>) {
     Object.assign(this, init);
