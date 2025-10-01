a46e5ae68ec58c351074556d54854912ca8665d3
Carolina Fantini <carolfantinirocha@gmail.com>
2025-10-01 00:26:30 -0300

    Add novos métodos de consulta a API

--- src/app/services/reading-journal-service.ts
+++ src/app/services/reading-journal-service.ts
@@ -3,6 +3,8 @@ import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../models/reading-journal/book';
+import { Author } from '../models/reading-journal/author';
+import { Series } from '../models/reading-journal/series';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +17,31 @@ export class ReadingJournalService {
     return this.http.get<Book[]>(`${this.apiUrl}/books/getall`);
   }
 
+  editBook(book: Book): Observable<boolean> {
+    return this.http.patch<boolean>(`${this.apiUrl}/book/edit`, book);
+  }
+
   createBook(book: Book): Observable<boolean> {
     return this.http.post<boolean>(`${this.apiUrl}/book/create`, book);
   }
 
   deleteBook(id: string): Observable<boolean> {
     return this.http.delete<boolean>(`${this.apiUrl}/book/delete/${id}`);
+  }
+
+  createAuthor(authorName: string): Observable<Author> {
+    return this.http.post<Author>(
+      `${this.apiUrl}/author/create`,
+      `"${authorName}"`,
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+
+  createSeries(seriesName: string): Observable<Series> {
+    return this.http.post<Series>(
+      `${this.apiUrl}/series/create`,
+      `"${seriesName}"`,
+      { headers: { 'Content-Type': 'application/json' } }
+    );
   }
 }
