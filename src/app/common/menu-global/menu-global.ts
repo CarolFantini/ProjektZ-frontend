9b11b887d79864545af23d4a62c9e527361235e0
Carolina Fantini <carolfantinirocha@gmail.com>
2025-10-14 13:07:36 -0300

    Implementando menu do PrimeNg e uniformizando cor entre os componentes

--- src/app/common/menu-global/menu-global.ts
+++ src/app/common/menu-global/menu-global.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-menu-global',
-  imports: [],
+  imports: [ButtonModule, Menubar],
   templateUrl: './menu-global.html',
   styleUrl: './menu-global.scss'
 })
 export class MenuGlobal {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        url: 'pages/home',
+      },
+      {
+        label: 'Household Budget',
+        icon: 'pi pi-chart-line',
+        url: 'pages/household-budget'
+      },
+      {
+        label: 'Reading Journal',
+        icon: 'pi pi-book',
+        url: 'pages/reading-journal'
+      },
+      {
+        label: 'Yearly OKRs Tracker',
+        icon: 'pi pi-compass',
+        items: [
+          {
+            label: '2025 Yearly OKRs Tracker',
+            url: ''
+          },
+          {
+            label: '2024 Yearly OKRs Tracker',
+            url: ''
+          }
+        ]
+      }
+    ]
+  }
 }
